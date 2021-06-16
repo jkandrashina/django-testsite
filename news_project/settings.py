38622699b28611dc9ef43f74798708730b917b12
jkandrashina <18037987+jkandrashina@users.noreply.github.com>
2021-06-16 15:06:43 +0500

    db and req.

--- news_project/settings.py
+++ news_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '*=40%6f5azy=!ty)in)zlt+@j%1lgy*dr0!qis1i#lbvrh5nr*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
